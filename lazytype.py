7242bc33bf9e35ea310466eb7f90cfc6c683fb03
Nigel A Pressey <112425060+RestingNigel@users.noreply.github.com>
2025-04-21 12:09:14 -0400

    library ideas

---
+++ lazytype.py
@@ -0,0 +1,10 @@
+# establish prefix
+# listen for i and trigger
+# delete prefix and trigger and type 
+#import keyboard
+#import pyperclip
+#import time
+# PyAutoGUI? I like this
+prefix = "--"
+macone = "p"
+maconeout = "hello"
